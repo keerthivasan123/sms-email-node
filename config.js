a1e82a393dfdcccbcdcc867a18914a56c0ea192d
keerthivasan123 <keerthivasan287@gmail.com>
2019-08-04 15:53:48 +0530

    region changed

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     aws_credentions: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: "us-west-2"
+      region: "us-east-1"
     }
   };
