61029ee313e9ff1be376d03f639ac54bb68f3115
keerthivasan123 <keerthivasan287@gmail.com>
2019-08-08 22:33:14 +0530

    fixed credintials problem

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 // Importing node_modules
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config');
-var aws     = require('aws-sdk');
 // Getting environment variables
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 // Starting nodeJs 
 const app = express();
 app.listen(port, () => console.log(`Server listening in port ${port}`));
-var ses = require('node-ses')
-  , client = ses.createClient({ key: 'key', secret: 'secret' });
- 
-// Give SES the details and let it construct the message for you.
-client.sendEmail({
-   to: 'keerthivasan287@gmail.com'
- , from: 'keerthivasan287@gmail.com'
- , cc: 'theWickedWitch@nerds.net'
- , bcc: ['canAlsoBe@nArray.com', 'forrealz@.org']
- , subject: 'greetings'
- , message: 'your <b>message</b> goes here'
- , altText: 'plain text'
-}, function (err, data, res) {
-    if(err) console.log(err);
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+
+// Create unique bucket name
+var bucketName = 'node-sdk-sample-' + uuid.v4();
+// Create name for uploaded object key
+var keyName = require('./config');
+
+// Create a promise on S3 service object
+var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+
+// Handle promise fulfilled/rejected states
+bucketPromise.then(
+  function(data) {
+    // Create params for putObject call
+    var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    // Create object upload promise
+    var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+    uploadPromise.then(
+      function(data) {
+        console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+      });
+}).catch(
+  function(err) {
+    console.error(err, err.stack);
 });
 
 
