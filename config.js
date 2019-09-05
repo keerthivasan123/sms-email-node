8e79953f0cf1db73e217236ee87ad9d73440a878
keerthivasan123 <keerthivasan287@gmail.com>
2019-09-05 22:22:30 +0530

    removerd config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-module.exports = {
-    aws_credentions: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: "us-east-1"
-    }
-  };
